7c7fcc7dabeac0db4814a44a26160deb61565e8d
tryingtolearn11 <damiensingh17@gmail.com>
2021-05-28 12:57:28 -0400

    new

--- server.go
+++ server.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+type DNA struct {
+	Phrase []byte
+}
+
+// create a DNA
+//func createDNA(p []byte) (dna DNA) {}
+
+/*
 func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
 }
@@ -13,3 +21,4 @@ func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":5000", nil)
 }
+*/
