a42f07bde4087729be475bae0f994daae3b31fcd
tryingtolearn11 <damiensingh17@gmail.com>
2021-05-28 17:20:37 -0400

    working solution

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"math/rand"
 	"time"
@@ -8,7 +9,7 @@ import (
 
 type DNA struct {
 	Phrase  []byte
-	Fitness float32
+	Fitness float64
 }
 
 // create a DNA
@@ -26,17 +27,11 @@ func createDNA(p []byte) (dna DNA) {
 		Fitness: measureFitness(p, dummy),
 	}
 
-	// convert back to string just to test
-	//myString := string(dna.Phrase)
-
-	//fmt.Println(dummy)
-	//fmt.Println(myString)
-	//fmt.Println("Fitness : ", dna.Fitness)
 	return
 }
 
 // need to measure the fitness
-func measureFitness(p []byte, dummy []byte) (fitness float32) {
+func measureFitness(p []byte, dummy []byte) (fitness float64) {
 	score := 0
 	for i := 0; i < len(p); i++ {
 		if p[i] == dummy[i] {
@@ -44,7 +39,7 @@ func measureFitness(p []byte, dummy []byte) (fitness float32) {
 		}
 	}
 
-	fitness = float32(score) / float32(len(p))
+	fitness = float64(score) / float64(len(p))
 
 	return
 }
@@ -60,66 +55,48 @@ func createPopulation(p []byte) (population []DNA) {
 }
 
 // Build Mating Pool
-// TODO: Might be bugged, remember size of pool is > 100
-// MIGHT BE FIXED (TODO: NOTE that 0 DNA objects make it through into the pool
-// -keep them out)
-func generateMatingPool(p []byte) {
-	population := createPopulation(p)
-	for i := 0; i < len(population); i++ {
-		fmt.Println(population[i].Fitness)
-	}
+func generateMatingPool(p []byte, population []DNA, fit float64) []DNA {
 
 	var matingPool []DNA
 	count := 0
 	for j := 0; j < len(population); j++ {
 		// find percentage score
-		n := int(population[j].Fitness * 100)
+		n := int((population[j].Fitness / fit) * 100)
 		count += n
 		// add this dna N number of times
 		for k := 0; k < n; k++ {
 			matingPool = append(matingPool, population[j])
 		}
 	}
-	fmt.Println("COUNT : ", count)
 
-	// mating pool
-	/*
-		for i := 0; i < len(matingPool); i++ {
-			fmt.Println(matingPool[i])
-		}
-	*/
+	newPool := make([]DNA, len(population))
 
-	fmt.Println("Size of matingPool : ", len(matingPool))
+	for i := 0; i < len(population); i++ {
 
-	// parents selection : 2 parents to mimic human reproduction
-	one := rand.Intn(len(matingPool))
-	two := rand.Intn(len(matingPool))
-	fmt.Println("PARENTS 1 & 2 : ", one, two)
+		// parents selection : 2 parents to mimic human reproduction
+		one := rand.Intn(len(matingPool))
+		two := rand.Intn(len(matingPool))
 
-	//TODO: Make sure parents are NOT the same values
+		//TODO: Make sure parents are NOT the same values
 
-	parent1 := matingPool[one]
-	parent2 := matingPool[two]
+		parent1 := matingPool[one]
+		parent2 := matingPool[two]
 
-	child := crossover(parent1, parent2)
-	child.mutate()
-	// just to measure the child's fitness
-	child.Fitness = measureFitness(p, child.Phrase)
-
-	fmt.Println("PARENTS 1 & 2 : ", parent1, parent2)
-	fmt.Println("PARENTS 1 & 2 : ", string(parent1.Phrase), " | ", string(parent2.Phrase))
-	fmt.Println("Child DNA : ", child)
-	fmt.Println("Child DNA : ", string(child.Phrase))
+		child := crossover(parent1, parent2)
+		child.mutate()
+		// just to measure the child's fitness
+		child.Fitness = measureFitness(p, child.Phrase)
+		newPool[i] = child
+	}
+	return newPool
 
 }
 
 // crossover to generate the child DNA
 func crossover(p1 DNA, p2 DNA) (child DNA) {
-	fmt.Println(p1, p2)
-
 	// get random midpoint
 	midpoint := rand.Intn(len(p1.Phrase))
-	fmt.Println("midpoint ", midpoint)
+	//fmt.Println("midpoint ", midpoint)
 
 	// initialize child phrase size
 	child.Phrase = make([]byte, len(p1.Phrase))
@@ -142,29 +119,42 @@ func (child *DNA) mutate() {
 	for i := 0; i < len(child.Phrase); i++ {
 
 		if rand.Float64() < mutateRate {
-			fmt.Println("CHILD MUTATED")
 			child.Phrase[i] = byte(rand.Intn(95) + 32)
 
 		}
 	}
 }
 
+func successor(population []DNA) DNA {
+	position := 0
+	model := 0.0
+	for i := 0; i < len(population); i++ {
+		if population[i].Fitness > model {
+			model = population[i].Fitness
+			position = i
+		}
+	}
+	return population[position]
+}
+
 func main() {
 	// for constant random numbers
 	rand.Seed(time.Now().UTC().UnixNano())
+	match := false
+	s := []byte("to be or not to be")
+	population := createPopulation(s)
 
-	// test
-	//s := []byte("to be or not to be")
+	for !match {
+		best := successor(population)
+		fmt.Printf("\r best fitness : %2f", best.Fitness)
+		fmt.Println("Phrase : ", string(best.Phrase))
 
-	s := []byte("w,qI8Te'$/Z'{&>d98")
-	generateMatingPool(s)
-	//createDNA(s)
+		if bytes.Compare(best.Phrase, s) == 0 {
+			match = true
+		} else {
+			peak := best.Fitness
+			population = generateMatingPool(s, population, peak)
 
-	/*
-		t := []byte("to be dr vot do be")
-		fmt.Println(len(s), len(t))
-		test_fitness := measureFitness(s, t)
-		fmt.Println("test_fitness : ", test_fitness)
-	*/
-
+		}
+	}
 }
