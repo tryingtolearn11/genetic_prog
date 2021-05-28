f266b87841dff26630de88160509e5cf72c3afce
tryingtolearn11 <damiensingh17@gmail.com>
2021-05-28 15:26:30 -0400

    mating pool

--- server.go
+++ server.go
@@ -25,11 +25,11 @@ func createDNA(p []byte) (dna DNA) {
 	}
 
 	// convert back to string just to test
-	myString := string(dna.Phrase)
+	//myString := string(dna.Phrase)
 
 	//fmt.Println(dummy)
-	fmt.Println(myString)
-	fmt.Println("Fitness : ", dna.Fitness)
+	//fmt.Println(myString)
+	//fmt.Println("Fitness : ", dna.Fitness)
 	return
 }
 
@@ -50,25 +50,53 @@ func measureFitness(p []byte, dummy []byte) (fitness float32) {
 	return
 }
 
-func createPopulation(p []byte) {
-	population := []DNA{}
+func createPopulation(p []byte) (population []DNA) {
 	for i := 0; i < 100; i++ {
 		organism := createDNA(p)
 		population = append(population, organism)
 	}
 
 	fmt.Println("Size of Population : ", len(population))
-
+	return population
 }
 
 // Build Mating Pool
+// TODO: Might be bugged, remember size of pool is > 100
+func generateMatingPool(p []byte) {
+	population := createPopulation(p)
+	for i := 0; i < len(population); i++ {
+		fmt.Println(population[i].Fitness)
+	}
+
+	matingPool := make([]DNA, len(population))
+
+	for j := 0; j < len(population); j++ {
+		// find percentage score
+		n := int(population[j].Fitness * 100)
+		fmt.Print(" N : ", n)
+		// add this dna N number of times
+		for k := 0; k < n; k++ {
+			matingPool = append(matingPool, population[j])
+		}
+	}
+
+	// mating pool
+	/*
+		for i := 0; i < len(matingPool); i++ {
+			fmt.Println(matingPool[i])
+		}
+	*/
+
+	fmt.Println("Size of matingPool : ", len(matingPool))
+
+}
 
 func main() {
 	// test
 	//s := []byte("to be or not to be")
 	s := []byte("w,qI8Te'$/Z'{&>d98")
+	generateMatingPool(s)
 	//createDNA(s)
-	createPopulation(s)
 
 	/*
 		t := []byte("to be dr vot do be")
