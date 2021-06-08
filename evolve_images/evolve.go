5feff156722116a03da29976cc19078233720e6e
tryingtolearn11 <damiensingh17@gmail.com>
2021-06-08 15:58:55 -0400

    img

--- evolve_images/evolve.go
+++ evolve_images/evolve.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/llgcode/draw2d/draw2dimg"
 	"image"
 	"image/color"
+	"image/png"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -28,12 +31,57 @@ type Entity struct {
 	DNA     *image.RGBA
 }
 
+func display(width int, height int, circle_array []Circle) (i *image.RGBA) {
+	end := image.NewRGBA(image.Rect(0, 0, width, height))
+	gc := draw2dimg.NewGraphicContext(end)
+
+	for _, c := range circle_array {
+		gc.SetFillColor(c.Color)
+		gc.MoveTo(float64(c.X), float64(c.Y))
+		gc.ArcTo(float64(c.X), float64(c.Y), float64(c.Radius), float64(c.Radius), 0, 6.283185)
+		gc.Close()
+		gc.Fill()
+	}
+	return end
+}
+
+// load the parent image
+func loadImg(filePath string) *image.RGBA {
+	img, err := os.Open(filePath)
+	defer img.Close()
+	if err != nil {
+		fmt.Println("Error reading File")
+	}
+
+	pic, _, err := image.Decode(img)
+	if err != nil {
+		fmt.Println("err decoding file")
+	}
+	return pic.(*image.RGBA)
+}
+
+// where to save generated image
+func saveImg(filePath string, rgba *image.RGBA) {
+	img, err := os.Create(filePath)
+	defer img.Close()
+	if err != nil {
+		fmt.Println("Err creating File")
+	}
+	png.Encode(img, rgba.SubImage(rgba.Rect))
+}
+
 // going to pass randomized values for x, y and r here
-func (c *Circle) generateRandomizedCircle(width int, height int, radius int) (circle Circle) {
+func generateRandomizedCircle(width int, height int, radius int) (circle Circle) {
+	// TODO: try to aim for semi-transparent
+	r := uint8(rand.Intn(255))
+	g := uint8(rand.Intn(255))
+	b := uint8(rand.Intn(255))
+	a := uint8(rand.Intn(255))
 	circle = Circle{
 		X:      width,
 		Y:      height,
 		Radius: radius,
+		Color:  color.RGBA{r, g, b, a},
 	}
 	return
 }
@@ -46,7 +94,13 @@ func generateEntity(i *image.RGBA) (entity Entity) {
 		width := rand.Intn(i.Rect.Dx())
 		height := rand.Intn(i.Rect.Dy())
 		r := rand.Intn(circleSize)
-		circle_array[k].generateRandomizedCircle(width, height, r)
+		circle_array[k] = generateRandomizedCircle(width, height, r)
+	}
+
+	entity = Entity{
+		Circles: circle_array,
+		Fitness: 0,
+		DNA:     display(i.Rect.Dx(), i.Rect.Dy(), circle_array),
 	}
 
 	return
@@ -56,5 +110,11 @@ func generateEntity(i *image.RGBA) (entity Entity) {
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
+
 	fmt.Println("this is a test :D ")
+
+	img := loadImg("./test_imgs/clown.png")
+	test_img := generateEntity(img)
+	saveImg("/evolved_clown.png", test_img.DNA)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ga/vistwitch
 
 go 1.13
+
+require github.com/llgcode/draw2d v0.0.0-20210313082411-577c1ead272a
