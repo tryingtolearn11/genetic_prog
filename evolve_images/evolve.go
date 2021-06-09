d75dbdcd9ddbbe6ae57a9859146bb667749c2d18
tryingtolearn11 <damiensingh17@gmail.com>
2021-06-09 14:26:11 -0400

    another 2d library

--- evolve_images/evolve.go
+++ evolve_images/evolve.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/llgcode/draw2d/draw2dimg"
+	"github.com/fogleman/gg"
 	"image"
-	"image/color"
+	//"image/color"
 	"image/png"
+	"math"
 	"math/rand"
 	"os"
 	"time"
 )
 
+/*
 // constants
 var number_of_circles = 130
 var circleSize = 15
@@ -23,6 +25,7 @@ type Circle struct {
 	Color  color.Color
 }
 
+
 // Need to define an entity
 // will be composed of a slice of circles
 type Entity struct {
@@ -33,42 +36,11 @@ type Entity struct {
 
 func display(width int, height int, circle_array []Circle) (i *image.RGBA) {
 	end := image.NewRGBA(image.Rect(0, 0, width, height))
-	gc := draw2dimg.NewGraphicContext(end)
+	dc := gg.NewContext(width, height)
 
-	for _, c := range circle_array {
-		gc.SetFillColor(c.Color)
-		gc.MoveTo(float64(c.X), float64(c.Y))
-		gc.ArcTo(float64(c.X), float64(c.Y), float64(c.Radius), float64(c.Radius), 0, 6.283185)
-		gc.Close()
-		gc.Fill()
-	}
 	return end
 }
 
-// load the parent image
-func loadImg(filePath string) *image.RGBA {
-	img, err := os.Open(filePath)
-	defer img.Close()
-	if err != nil {
-		fmt.Println("Error reading File")
-	}
-
-	pic, _, err := image.Decode(img)
-	if err != nil {
-		fmt.Println("err decoding file")
-	}
-	return pic.(*image.RGBA)
-}
-
-// where to save generated image
-func saveImg(filePath string, rgba *image.RGBA) {
-	img, err := os.Create(filePath)
-	defer img.Close()
-	if err != nil {
-		fmt.Println("Err creating File", err)
-	}
-	png.Encode(img, rgba.SubImage(rgba.Rect))
-}
 
 // going to pass randomized values for x, y and r here
 func generateRandomizedCircle(width int, height int, radius int) (circle Circle) {
@@ -105,16 +77,84 @@ func generateEntity(i *image.RGBA) (entity Entity) {
 
 	return
 }
-
+*/
 // TODO : try to create an entity image and render it to webpage
+
+// load the parent image
+func loadImg(filePath string) *image.RGBA {
+	img, err := os.Open(filePath)
+	defer img.Close()
+	if err != nil {
+		fmt.Println("Error reading File")
+	}
+
+	pic, _, err := image.Decode(img)
+	if err != nil {
+		fmt.Println("err decoding file")
+	}
+	return pic.(*image.RGBA)
+}
+
+// where to save generated image
+func saveImg(filePath string, rgba *image.RGBA) {
+	img, err := os.Create(filePath)
+	defer img.Close()
+	if err != nil {
+		fmt.Println("Err creating File", err)
+	}
+	png.Encode(img, rgba.SubImage(rgba.Rect))
+}
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func Polygon(n int) []Point {
+	result := make([]Point, n)
+	for i := 0; i < n; i++ {
+		a := float64(i)*2*math.Pi/float64(n) - math.Pi/2
+		result[i] = Point{math.Cos(a), math.Sin(a)}
+	}
+	return result
+}
+
+func display(width int, height int) (i *image.RGBA) {
+	const S = 100
+	const W = 1200
+	const H = 120
+	end := image.NewRGBA(image.Rect(0, 0, width, height))
+	dc := gg.NewContext(width, height)
+	n := 5
+	points := Polygon(n)
+	for x := S / 2; x < W; x += S {
+		dc.Push()
+		s := rand.Float64()*S/4 + S/4
+		dc.Translate(float64(x), H/2)
+		dc.Rotate(rand.Float64() * 2 * math.Pi)
+		dc.Scale(s, s)
+		for i := 0; i < n+1; i++ {
+			index := (i * 2) % n
+			p := points[index]
+			dc.LineTo(p.X, p.Y)
+		}
+		dc.SetLineWidth(10)
+		dc.SetHexColor("#FFCC00")
+		dc.StrokePreserve()
+		dc.SetHexColor("#FFE43A")
+		dc.Fill()
+		dc.Pop()
+
+	}
+	dc.SavePNG("out.png")
+	return end
+}
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-
 	fmt.Println("this is a test :D ")
-
 	img := loadImg("./test_imgs/clown.png")
-	test_img := generateEntity(img)
-	saveImg("../static/pictures/"+"clown2.png", test_img.DNA)
-
+	result := display(img.Rect.Dx(), img.Rect.Dy())
+	//test_img := generateEntity(img)
+	saveImg("../static/pictures/"+"result.png", result)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module ga/vistwitch
 
 go 1.13
 
-require github.com/llgcode/draw2d v0.0.0-20210313082411-577c1ead272a
+require (
+	github.com/fogleman/gg v1.3.0
+	github.com/llgcode/draw2d v0.0.0-20210313082411-577c1ead272a
+)
